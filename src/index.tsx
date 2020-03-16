e030c02cad1204a9107b7fb96cbad138f4a3a013
qudelin <levinqdl@gmail.com>
2020-03-17 00:02:57 +0800

    style: apply css lib
    1. linaria setup
    2. reset default css by sanitize.css

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,20 @@
 /// <reference types="react/experimental" />
 import React from "react"
 import ReactDOM from "react-dom"
+import { css } from "linaria"
+import "sanitize.css"
 
 const root = document.createElement("div")
 document.body.appendChild(root)
 
-const App = () => <div>Hello World!</div>
+const App = () => (
+  <div
+    className={css`
+      height: 100vh;
+    `}
+  >
+    Hello World!
+  </div>
+)
 
 ReactDOM.createRoot(root).render(<App />)
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,50 @@
 /* eslint-env node */
 const HtmlPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
   entry: "./src/index.tsx",
+  mode: process.env.NODE_ENV,
   module: {
     rules: [
       {
         test: /\.tsx$/,
-        use: {
-          loader: "babel-loader"
-        }
+        use: [
+          {
+            loader: "babel-loader"
+          },
+          {
+            loader: "linaria/loader",
+            options: {
+              sourceMap: process.env.NODE_ENV !== "production"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV !== "production"
+            }
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: process.env.NODE_ENV !== "production",
+              importLoaders: 1
+            }
+          }
+        ]
       }
     ]
   },
-  plugins: [new HtmlPlugin({ favicon: "favicon.ico" })]
+  plugins: [
+    new HtmlPlugin({ favicon: "favicon.ico" }),
+    new MiniCssExtractPlugin({
+      filename: "styles.css"
+    })
+  ]
 }
